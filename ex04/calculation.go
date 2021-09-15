652597973a534ec0c5b3863bbd40b5198abfe72b
dnakano <dnakano@student.42tokyo.jp>
2021-09-15 23:48:10 +0900

    add ex04

---
+++ ex04/calculation.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const ERROR_MSG = "Argument is invalid"
+
+func main() {
+	s, ok := calulationStr(os.Args)
+	if !ok {
+		fmt.Println(ERROR_MSG)
+		os.Exit(1)
+	}
+	fmt.Print(s)
+}
+
+func calulationStr(args []string) (string, bool) {
+	if len(args) != 3 {
+		return "", false
+	}
+
+	a, err := strconv.ParseInt(args[1], 10, 64)
+	if err != nil {
+		return "", false
+	}
+
+	b, err := strconv.ParseInt(args[2], 10, 64)
+	if err != nil {
+		return "", false
+	}
+
+	s := fmt.Sprintf("sum: %d\n", a+b)
+	s += fmt.Sprintf("difference: %d\n", a-b)
+	s += fmt.Sprintf("product: %d\n", a*b)
+	s += fmt.Sprintf("quotient: %d\n", a/b)
+	return s, true
+}
